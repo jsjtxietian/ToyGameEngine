6a35c962b52d6727361fd0e1bc043caefc08c2d0
TianXieUnity <tian.xie@unity3d.com>
2020-08-12 17:52:55 +0800

    add simple event system

--- QAQ/src/QAQ/Application.cpp
+++ QAQ/src/QAQ/Application.cpp
@@ -1,4 +1,6 @@
 #include "Application.h"
+#include "QAQ/Events/ApplicationEvent.h"
+#include "QAQ/Log.h"
 
 namespace QAQ
 {
@@ -12,6 +14,15 @@ namespace QAQ
 
 	void Application::Run()
 	{
+		WindowResizeEvent e(1280, 720);
+		if (e.IsInCategory(EventCategoryApplication))
+		{
+			QAQ_TRACE(e);
+		}
+		if (e.IsInCategory(EventCategoryInput))
+		{
+			QAQ_TRACE(e);
+		}
 		while (true);
 	}
 
--- QAQ/src/QAQ/Core.h
+++ QAQ/src/QAQ/Core.h
@@ -9,3 +9,5 @@
 #else
 	#error Only Support Windows!
 #endif
+
+#define BIT(x) (1 << x) 
