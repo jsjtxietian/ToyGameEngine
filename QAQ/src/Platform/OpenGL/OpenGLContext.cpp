02f9bbce4d37a7fd3f99c68e5c737f81148f7fe8
jsjtxietian <jsjtxietian@outlook.com>
2020-09-10 14:09:35 +0800

    maintain

--- QAQ/src/Platform/OpenGL/OpenGLContext.cpp
+++ QAQ/src/Platform/OpenGL/OpenGLContext.cpp
@@ -26,14 +26,7 @@ namespace QAQ {
 		QAQ_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
 		QAQ_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
 
-#ifdef QAQ_ENABLE_ASSERTS
-		int versionMajor;
-		int versionMinor;
-		glGetIntegerv(GL_MAJOR_VERSION, &versionMajor);
-		glGetIntegerv(GL_MINOR_VERSION, &versionMinor);
-
-		QAQ_CORE_ASSERT(versionMajor > 4 || (versionMajor == 4 && versionMinor >= 5), "QAQ requires at least OpenGL version 4.5!");
-#endif
+		QAQ_CORE_ASSERT(GLVersion.major > 4 || (GLVersion.major == 4 && GLVersion.minor >= 5), "Hazel requires at least OpenGL version 4.5!");
 	}
 
 	void OpenGLContext::SwapBuffers()
--- QAQ/src/QAQ/Renderer/Renderer2D.cpp
+++ QAQ/src/QAQ/Renderer/Renderer2D.cpp
@@ -94,7 +94,7 @@ namespace QAQ {
 		s_Data.TextureShader->Bind();
 		s_Data.TextureShader->SetIntArray("u_Textures", samplers, s_Data.MaxTextureSlots);
 
-		// Set all texture slots to 0
+		// Set first  texture slots to 0
 		s_Data.TextureSlots[0] = s_Data.WhiteTexture;
 
 		s_Data.QuadVertexPositions[0] = { -0.5f, -0.5f, 0.0f, 1.0f };
@@ -198,7 +198,7 @@ namespace QAQ {
 		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position)
 			* glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
 
-		DrawQuad(transform, texture, tilingFactor);
+		DrawQuad(transform, texture, tilingFactor, tintColor);
 	}
 
 	void Renderer2D::DrawQuad(const glm::mat4& transform, const glm::vec4& color)
@@ -246,7 +246,7 @@ namespace QAQ {
 		float textureIndex = 0.0f;
 		for (uint32_t i = 1; i < s_Data.TextureSlotIndex; i++)
 		{
-			if (*s_Data.TextureSlots[i].get() == *texture.get())
+			if (*s_Data.TextureSlots[i] == *texture)
 			{
 				textureIndex = (float)i;
 				break;
@@ -330,7 +330,7 @@ namespace QAQ {
 	{
 		DrawRotatedQuad({ position.x, position.y, 0.0f }, size, rotation, color);
 	}
-	
+
 	void Renderer2D::DrawRotatedQuad(const glm::vec3& position, const glm::vec2& size, float rotation, const glm::vec4& color)
 	{
 		QAQ_PROFILE_FUNCTION();
