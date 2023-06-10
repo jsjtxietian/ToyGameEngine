3f969cae988a2e7c2c1e921c487dd697ebc82d31
jsjtxietian <jsjtxietian@outlook.com>
2023-06-10 20:32:57 +0800

    drag texture

--- QAQ/src/QAQ/Scene/Components.h
+++ QAQ/src/QAQ/Scene/Components.h
@@ -8,6 +8,7 @@
 
 #include "SceneCamera.h"
 #include "ScriptableEntity.h"
+#include "QAQ/Renderer/Texture.h"
 
 namespace QAQ {
 
@@ -45,6 +46,8 @@ namespace QAQ {
 	struct SpriteRendererComponent
 	{
 		glm::vec4 Color{ 1.0f, 1.0f, 1.0f, 1.0f };
+		Ref<Texture2D> Texture;
+		float TilingFactor = 1.0f;
 
 		SpriteRendererComponent() = default;
 		SpriteRendererComponent(const SpriteRendererComponent&) = default;
