0d06f4d4c3f1d0e70bc3c728851c5966af76558c
jsjtxietian <jsjtxietian@outlook.com>
2023-06-13 21:55:25 +0800

    Stability & bug fixes

--- QAQNut/src/Panels/SceneHierarchyPanel.cpp
+++ QAQNut/src/Panels/SceneHierarchyPanel.cpp
@@ -338,7 +338,11 @@ namespace QAQ {
 				{
 					const wchar_t* path = (const wchar_t*)payload->Data;
 					std::filesystem::path texturePath = std::filesystem::path(g_AssetPath) / path;
-					component.Texture = Texture2D::Create(texturePath.string());
+					Ref<Texture2D> texture = Texture2D::Create(texturePath.string());
+					if (texture->IsLoaded())
+						component.Texture = texture;
+					else
+						QAQ_ERROR("Could not load texture {0}", texturePath.filename().string());
 				}
 				ImGui::EndDragDropTarget();
 			}
