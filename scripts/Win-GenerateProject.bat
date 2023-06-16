41ae9814d6c0af14466ea665ca3a7baeb9ff687b
jsjtxietian <jsjtxietian@outlook.com>
2023-06-16 22:55:13 +0800

    trigger build from vscode

--- scripts/Win-GenerateProject.bat
+++ scripts/Win-GenerateProject.bat
@@ -1,5 +1,7 @@
 @echo off
 pushd %~dp0\..\
 call vendor\premake\bin\premake5.exe vs2022
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+msbuild ToyEngine.sln -target:QAQNut
 popd
 PAUSE
