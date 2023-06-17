e109604532c67448bf8b6a4944541b29b5c188cc
jsjtxietian <jsjtxietian@outlook.com>
2023-06-17 23:41:01 +0800

    update bat

--- scripts/Win-GenerateProject.bat
+++ scripts/Win-GenerateProject.bat
@@ -4,4 +4,4 @@ call vendor\premake\bin\premake5.exe vs2022
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 msbuild ToyEngine.sln -target:QAQNut
 popd
-PAUSE
+@REM PAUSE
