33dd2a507504a9c21d8337786d71df836d5081cf
jsjtxietian <jsjtxietian@outlook.com>
2023-06-19 21:39:57 +0800

    variadic template functions

--- QAQ/src/QAQ/Scene/Components.h
+++ QAQ/src/QAQ/Scene/Components.h
@@ -150,4 +150,14 @@ namespace QAQ {
 		CircleCollider2DComponent(const CircleCollider2DComponent&) = default;
 	};
 
+	template<typename... Component>
+	struct ComponentGroup
+	{
+	};
+
+	using AllComponents = 
+		ComponentGroup<TransformComponent, SpriteRendererComponent,
+			CircleRendererComponent, CameraComponent, NativeScriptComponent,
+			Rigidbody2DComponent, BoxCollider2DComponent, CircleCollider2DComponent>;
+
 }
