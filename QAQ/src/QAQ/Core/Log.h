438e6462108638bc5484cbd9ab61a1a469985400
jsjtxietian <jsjtxietian@outlook.com>
2023-06-23 23:14:57 +0800

    add log for glm

--- QAQ/src/QAQ/Core/Log.h
+++ QAQ/src/QAQ/Core/Log.h
@@ -38,3 +38,25 @@ namespace QAQ {
 #define QAQ_WARN(...)          ::QAQ::Log::GetClientLogger()->warn(__VA_ARGS__)
 #define QAQ_ERROR(...)         ::QAQ::Log::GetClientLogger()->error(__VA_ARGS__)
 #define QAQ_CRITICAL(...)      ::QAQ::Log::GetClientLogger()->critical(__VA_ARGS__)
+
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include "glm/gtx/string_cast.hpp"
+
+template<typename OStream, glm::length_t L, typename T, glm::qualifier Q>
+inline OStream& operator<<(OStream& os, const glm::vec<L, T, Q>& vector)
+{
+	return os << glm::to_string(vector);
+}
+
+template<typename OStream, glm::length_t C, glm::length_t R, typename T, glm::qualifier Q>
+inline OStream& operator<<(OStream& os, const glm::mat<C, R, T, Q>& matrix)
+{
+	return os << glm::to_string(matrix);
+}
+
+template<typename OStream, typename T, glm::qualifier Q>
+inline OStream& operator<<(OStream& os, glm::qua<T, Q> quaternio)
+{
+	return os << glm::to_string(quaternio);
+}
