3a9dd34bad6c61b14bab22a36e7f36834ca9880f
jsjtxietian <jsjtxietian@outlook.com>
2023-06-25 22:29:15 +0800

    update build file for seperate dll

--- scripts/Win-GenerateProject.bat
+++ scripts/Win-GenerateProject.bat
@@ -3,6 +3,7 @@ pushd %~dp0\..\
 call vendor\premake\bin\premake5.exe vs2022
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 msbuild ToyEngine.sln -target:CSharpEngine
+msbuild QAQNut\SandboxProject\Assets\Scripts\Sandbox.sln -target:Sandbox
 msbuild ToyEngine.sln -target:QAQNut
 popd
 @REM PAUSE
